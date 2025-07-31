dcf51bb53dd21cd2d85c0c574306ee4af44cbf38
Abdul Rehman kiyani <154977405+abdul-rehman07@users.noreply.github.com>
2025-07-31 16:24:26 +0500

    foam action

---
+++ foam-action/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './Components/Header.jsx';
+import Signup from './Components/Signup.jsx';
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Signup />
+      </main>
+    </>
+  );
+}
+
+export default App;
